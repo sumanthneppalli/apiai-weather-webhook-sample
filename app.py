e754d1d4f8624816d17d95dd65a28cc31f0504f2
sumanthneppalli <nsumanth3@gmail.com>
2016-12-24 01:34:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,54 +31,27 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
-    result = urllib.urlopen(yql_url).read()
+    baseurl = "https://www.quandl.com/api/v3/datasets/WIKI/FB.json?api_key=yghN7Qe-MTpHT5ZWCpV-"
+    result = urllib.urlopen(baseurl).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res
 
 
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
-
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
-
 def makeWebhookResult(data):
-    query = data.get('query')
+    query = data.get('dataset')
     if query is None:
         return {}
 
-    result = query.get('results')
+    result = query.get('dataset_code')
     if result is None:
         return {}
 
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
+  
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = result
 
     print("Response:")
     print(speech)
